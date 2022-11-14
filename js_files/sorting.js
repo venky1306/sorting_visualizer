b2087bc0ae7715a566ea23959b8a2e8475188eaf
venky1306 <47674519+venky1306@users.noreply.github.com>
2022-11-14 17:08:25 -0600

    addend barrs and new array functionality

---
+++ js_files/sorting.js
@@ -0,0 +1,48 @@
+function getRndInteger(max) {
+    return Math.floor(Math.random() * (max) ) + 1;
+}
+
+function deleteChild() {
+    var e = document.getElementById("bars");
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+}
+
+new_arr();
+
+document.getElementById("new_array").addEventListener("click", new_arr);
+
+function new_arr(){
+
+    deleteChild();
+
+    const nums = [];
+    let max = 50
+    for( let i=0;i<25;i++){
+        nums[i] = getRndInteger(max);
+    }
+
+    for(let i=0;i<25;i++){
+        var div_bars = document.createElement('div');
+        div_bars.innerText = nums[i];
+        document.getElementById("bars").appendChild(div_bars);
+        div_bars.classList.add("bars_style");
+        if(i&1==1){
+            div_bars.style.backgroundColor = 'red';
+        }
+        else{
+            div_bars.style.backgroundColor = 'green';
+        }
+        div_bars.style.height = (nums[i]*10+10).toString()+"px";
+        div_bars.style.width = "20px";
+        div_bars.style.margin = "2px";
+        
+        
+    }
+}
+
+
+
