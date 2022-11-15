07c0b2752627a43906689cdefcd21dba0d2f8f94
venky1306 <47674519+venky1306@users.noreply.github.com>
2022-11-15 00:21:55 -0600

    delay bugs fixed

--- js_files/bubble.js
+++ js_files/bubble.js
@@ -14,20 +14,23 @@ function swap(el1,el2){
 
 let count_of_bars = document.getElementById("bars").childElementCount;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 document.getElementById("bubble_sort").addEventListener("click", sort);
 
 async function sort(){
     let children = document.getElementById("bars").children;
-    console.log(typeof(children[2].innerHTML));
-    console.log(count_of_bars);
+    // console.log(typeof(children[2].innerHTML));
+    // console.log(count_of_bars);
     var i, j;
     for (i = 0; i < count_of_bars-1; i++){
         for (j = 0; j < count_of_bars-i-1; j++){
             children[j].style.background="red";
             children[j+1].style.background="red";
             if (parseInt(children[j].innerHTML) > parseInt(children[j+1].innerHTML)){
-                await new Promise(resolve => setTimeout(() => {resolve(), delay(2)}));
+                await sleep(75);
                 swap(children[j], children[j+1]);
                 let temp = children[j].innerHTML;
                 children[j].innerHTML = children[j+1].innerHTML;
