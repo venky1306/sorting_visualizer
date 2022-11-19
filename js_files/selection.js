5abc9ef7f549e12f55d99fed88e3a74462244f94
venky1306 <47674519+venky1306@users.noreply.github.com>
2022-11-19 02:29:05 -0600

    fixed bar colors miscoloring selection sort

--- js_files/selection.js
+++ js_files/selection.js
@@ -35,30 +35,38 @@ async function sort(){
     var i, j, min_idx;
     for (i = 0; i < count_of_bars-1; i++)
     {
-        let arr_size=document.querySelector("#bars_speed"); 
-        let no_of_bar=arr_size.value;
+        let no_of_bar=document.querySelector("#bars_speed").value;
         // Find the minimum element in unsorted array
         min_idx = i;
         children[i].style.background="red";
+        await sleep(160-no_of_bar);
         for (j = i + 1; j < count_of_bars; j++){
             children[j].style.background="red";
+            await sleep(160-no_of_bar);
             if (parseInt(children[j].innerHTML) < parseInt(children[min_idx].innerHTML)){
-                children[min_idx].style.background="blue";
+                if(min_idx!=i)
+                    children[min_idx].style.background="blue";
                 children[j].style.background="red";
-                await sleep(200-no_of_bar);
+                await sleep(160-no_of_bar);
                 min_idx = j;
             }
-            else
+            else{
+                await sleep(200-no_of_bar);
                 children[j].style.background="blue";
+            }
+                
         }
         // Swap the found minimum element with the first element
-        
-        await sleep(200-no_of_bar);
+
+        await sleep(160-no_of_bar);
         swap(children[min_idx], children[i]);
-        children[i].style.background="green";
         let temp = children[i].innerHTML;
         children[i].innerHTML = children[min_idx].innerHTML;
         children[min_idx].innerHTML = temp;
+        
+        children[min_idx].style.background="blue";
+        children[i].style.background="green";
+        
     }
     children[i].style.background="green";
 
