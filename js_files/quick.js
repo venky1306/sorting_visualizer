12481b9068479cb97ec0296bb4c022868d354d10
venky1306 <47674519+venky1306@users.noreply.github.com>
2022-11-24 05:29:10 -0600

    added delay sucessfully without exiting the function

--- js_files/quick.js
+++ js_files/quick.js
@@ -18,33 +18,45 @@ function sleep(ms) {
 let children = document.getElementById("bars").children;
 document.getElementById("quick_sort").addEventListener("click", sort);
 
+var speed = document.querySelector("#bars_speed").value;
 
-function partition(low, high) {
+
+
+async function partition(low, high) {
     let pivot = parseInt(children[high].innerHTML);
     let i = (low - 1);
   
     for (let j = low; j <= high - 1; j++) {
         if (parseInt(children[j].innerHTML) < pivot) {
             i++;
+            if(stoping_var) return;
+            await sleep(160-no_of_bar);
             swap(children[i], children[j]);
             let temp = children[j].innerHTML;
             children[j].innerHTML = children[i].innerHTML;
             children[i].innerHTML = temp;
+            await sleep(160-no_of_bar)
         }
     }
+    if(stoping_var) return;
+    no_of_bar=document.querySelector("#bars_speed").value;
+    
     swap(children[i + 1], children[high]);
     let temp = children[high].innerHTML;
     children[high].innerHTML = children[i+1].innerHTML;
     children[i+1].innerHTML = temp;
+    await sleep(160-no_of_bar);
     return (i + 1);
 }
 
-function qsort(low, high){
+
+async function qsort(low, high){
     // console.log(low);
     if (low < high) {
-        let pi = partition(low, high);
-        qsort(low, pi - 1);
-        qsort(pi + 1, high);
+        let pi = await partition(low, high);
+        await qsort(low, pi - 1);
+        if(stoping_var) return;
+        await qsort(pi + 1, high);
     }
 }
 
@@ -60,7 +72,7 @@ function sort(){
 
     let count_of_bars = document.getElementById("bars").childElementCount;
     qsort(0,count_of_bars-1);
-
+    stoping_var = false;
 
     document.getElementById("insertion_sort").removeAttribute('disabled');
     document.getElementById("merge_sort").removeAttribute('disabled');
